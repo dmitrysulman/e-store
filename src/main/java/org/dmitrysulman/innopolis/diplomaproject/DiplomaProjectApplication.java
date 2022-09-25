38ab7e52508ae6cb6c95ba18ab13ab83a64f4e40
Dmitry Sulman <dmitry.sulman@gmail.com>
2022-09-25 21:21:04 +0300

    First Commit

---
+++ src/main/java/org/dmitrysulman/innopolis/diplomaproject/DiplomaProjectApplication.java
@@ -0,0 +1,13 @@
+package org.dmitrysulman.innopolis.diplomaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dmitrysulman/innopolis/diplomaproject/DiplomaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dmitrysulman.innopolis.diplomaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
